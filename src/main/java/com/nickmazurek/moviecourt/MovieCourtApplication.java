bcffe22f153454a5f0a145f4d96f8b9d769b8a57
Nick <nickjoemaz@gmail.com>
2024-08-29 14:54:28 -0400

    first test init for group project

---
+++ src/main/java/com/nickmazurek/moviecourt/MovieCourtApplication.java
@@ -0,0 +1,13 @@
+package com.nickmazurek.moviecourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCourtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCourtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nickmazurek/moviecourt/MovieCourtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickmazurek.moviecourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCourtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
